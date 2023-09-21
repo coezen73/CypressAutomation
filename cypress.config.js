5f89d7e0d62c8627453557f9c91f83c5f72cb719
coezen73 <coezen73@gmail.com>
2023-09-21 14:28:43 +0200

    add: Configuraton Functions

--- cypress.config.js
+++ cypress.config.js
@@ -3,8 +3,12 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://practice.cydeo.com/',
+    video:false,         
+    // if we want to record a video-> video:true,
+    retries:2,
+    // if we want to run our testcase 3 times retries:2
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+    // implement node event listeners here
     },
   },
 });
