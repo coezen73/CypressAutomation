557cfaab272338d21f40d80b3ebbf702a20ce33d
coezen73 <coezen73@gmail.com>
2023-09-25 13:10:12 +0200

    add: button 2

--- cypress/e2e/buttons.cy.js
+++ cypress/e2e/buttons.cy.js
@@ -1,5 +1,5 @@
 /// <reference types="cypress" />
-describe('Find or Get Elements by Using Different Locators' , () => {
+describe('Button Actions' , () => {
 
     beforeEach (() => {
         // runs before each test case;
@@ -11,9 +11,17 @@ describe('Find or Get Elements by Using Different Locators' , () => {
     it('Check Different Button Actions', ()=> {
     // Select button with a text: 
         cy.contains('Button 2').should('be.visible').click();
-   //     (locating)     -     (verifying)     -     (acting)
-        cy.contains('Clicked on button two!').should('be.visible');
- 
+    //    (locating)     -     (verifying)     -     (acting)
+       
+        
+    /* TASK: Find element with 'class' attribute and create a list
+             Then select 3rd element from the list   */
+
+        cy.get('.btn.btn-primary').then(($buttons) => { //<-I created a list, and named as 'buttons'
+           cy.wrap($buttons).eq(2).click();      // <- In order to turn that list into cypress element,
+                                                 //  I use wrap command. eq(2)-> it is a list(3rd button)
+           cy.contains('Clicked on button three!').should('be.visible');    
+        })     
    
      }) 
    })
