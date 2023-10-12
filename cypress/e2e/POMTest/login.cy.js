3f6c6c67369baada7093d7af71b924d1c4409c0d
coezen73 <coezen73@gmail.com>
2023-10-12 11:18:08 +0200

    add: POM-test2

--- cypress/e2e/POMTest/login.cy.js
+++ cypress/e2e/POMTest/login.cy.js
@@ -1,19 +1,35 @@
-// in order to call our 'object' from support/pages/auth.js:
+// In order to call our 'object' from support/pages/auth.js:Direct object calling
 import { auth } from '../../support/pages/auth';
 import { navigateTo } from '../../support/pages/navigation';
+
+// Another Approach: Creating an object here,
+//  and make it reach all objects inside the targeted(auth.js) file
+const LoginLocators = require('../../support/pages/auth');
 
 describe('Auth: Login user with different ways', () => {
   beforeEach('Navigate to Login Page', () => {
     cy.clearAllCookies();
     navigateTo.loginPage(); // it is called from POM
   });
-  it('Happy Path: Using POM-login function', () => {
+  it.skip('Happy Path: Using POM FUNCTION', () => {
     // auth.login('harcoded variables');  --> Not a good way. Instead:
     cy.fixture('user').then((user) => {
       auth.login(user.user3.username, user.user3.password);
-      // Now we can verify the text by using our custom command in commands.js file:
-      cy.textExists('You logged into a secure area!');
-      auth.logout();
     });
+    // Now we can verify the text by using our custom command in commands.js file:
+    cy.textExists('You logged into a secure area!');
+    auth.logout();
+  });
+
+  it('Happy Path: Using POM LOCATORS', () => {
+    cy.fixture('user').then((user) => {
+      // Use LoginLocators object to call targeted objects(locators) in the 'auth.js file'
+      LoginLocators.locators.userName.type(user.user3.username);
+      LoginLocators.locators.password.type(user.user3.password);
+      LoginLocators.locators.submit.click();
+    });
+    // Now we can verify the text by using our custom command in commands.js file:
+    cy.textExists('You logged into a secure area!');
+    auth.logout();
   });
 });
--- cypress/support/pages/auth.js
+++ cypress/support/pages/auth.js
@@ -1,11 +1,12 @@
 /* 
     Difference than Java:
-            - class name doesn't have to be same with file name
-            - Can be more than one class in the file
+            - Class name doesn't have to be same with file name
+            - CAN be more than one class in the file
             - None of them has superiority over each other        */
 
+// our class will hold 2 functions:
+
 class Auth {
-  // our class will hold 2 functions:
   login(user_name, password) {
     cy.get('[name="username"]').type(user_name);
     cy.get('[name="password"]').type(password);
@@ -17,9 +18,34 @@ class Auth {
     cy.contains('Logout').should('be.visible').click();
   }
 }
-// Create the object of this class first:
+
+//--------------------------------------------------------------------------------
+
+/* Create another class here: 
+We can apply 'findBy' annotation of Selenium with Cypress to call the locators.
+The difference from Selenium is that we create functions here;  */
+
+class Locators {
+  get userName() {
+    // <- Webelement variable name
+    return cy.get('[name="username"]', { timeout: 10000 });
+    // I can define custom Timeout for a spesific element like here. It is dynamic.
+  }
+
+  get password() {
+    return cy.get('[name="password"]', { timeout: 10000 });
+  }
+
+  get submit() {
+    return cy.get('#wooden_spoon');
+  }
+}
+const locators = new Locators();
+
+// Create the object of these classes to be able to export:
 const auth = new Auth();
-// Then export 'auth' object:
+// Then export the objects:
 module.exports = {
   auth,
+  locators,
 };
